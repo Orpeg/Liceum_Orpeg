0fc16be115772e8be195a904686843f0420455d6
Wojciech Balcerzak <wbalcerz@gmail.com>
2020-03-25 11:38:33 +0100

    lekcja 7

---
+++ LO1_4_letnie/Lekcja7/pierwszy_python.py
@@ -0,0 +1,5 @@
+a = 2
+b = 3
+print(a)
+print(b)
+print("suma liczba a + b = ", a+b)
---
+++ LO1_4_letnie/Lekcja7/zolwik.py
@@ -0,0 +1,16 @@
+import turtle
+import math
+# pełny obót wynosi 360 stopni
+t = turtle.Turtle()
+t.color("blue")
+t.begin_fill()
+for i in range(11):
+    t.forward(80)
+    t.left(36)
+t.end_fill()
+turtle.done()
+
+# narysujemy teraz koło
+
+t.circle(150)
+
