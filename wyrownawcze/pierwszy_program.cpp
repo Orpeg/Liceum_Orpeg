f1ef01a5c3f6085c73c3cff95814976780bb0888
Wojciech Balcerzak <wbalcerz@gmail.com>
2020-03-27 20:54:06 +0100

    lekcja 4

---
+++ wyrownawcze/pierwszy_program.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+	
+	
+	
+int a = 10;
+float b = 3.1415;
+
+std::cout << "To jest pierwszy program w c++"<<"\n";
+std::cout << std::endl;
+
+std::cout << "zmienna a = " << a << std::endl;
+std::cout << "zmienna b = " << b << std::endl;
+
+
+
+
+return 0;
+	
+	
+	
+	
+	
+}
+
+
