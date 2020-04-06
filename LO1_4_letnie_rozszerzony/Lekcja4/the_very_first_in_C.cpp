dc6eaf8bad12eb6b83f536ebcbd113d3b6674c1d
Wojciech Balcerzak <wbalcerz@gmail.com>
2020-04-06 22:37:10 +0200

    lekcja 4

---
+++ LO1_4_letnie_rozszerzony/Lekcja4/the_very_first_in_C.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+//ten program jest kompilowany w Dev-C++
+
+std::cout << "to jest pierwszy program" << std::endl;
+std::cout <<"to jest druga linia " << "\n";
+//wprowadzanie wartoœci zmiennej
+int x;
+std::cout << "podaj liczbe calkowita" << std::endl;
+std::cin >> x;
+std::cout <<"podales liczbe x = " << x << "\n";
+
+
+return 0;
+}
---
+++ wyrownawcze/the_very_first.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main{
+
+std::cout << " To jest pierwszy program" << "\n";
+Std::cout <<"To jest druga linia programu " << std::endl;
+
+
+
+
+
+
+
+return 0;
+}
