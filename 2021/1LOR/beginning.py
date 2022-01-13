4174671ce74f039c2ecbdc760c05cd146f49ea28
Wojciech Balcerzakf <wbalcerz@gmail.com>
2022-01-13 20:45:25 +0100

    upda

---
+++ 2021/1LOR/beginning.py
@@ -0,0 +1,27 @@
+# programowanie w Pythonie
+# print("Witamy na lekcji Pythona")
+# zmienne
+# a = input('Jan masz na imię?  ')
+# print('Jak się masz, '+a, '? ')
+
+# x = int(input('podaj liczbę całkowitą x  '))
+# y = int(input('podaj liczbę całkowitą y '))
+
+# print('liczba x = ', x)
+# print('liczba y = ', y)
+
+# # data swapping
+# x,y = y,x
+# print('zamieniamy zmienne')
+
+# print('liczba x = ', x)
+# print('liczba y = ', y)
+
+# zmienna float
+
+z = 1.4142145
+
+print('pierwiastek z =', z)
+print('sprawdzamy kwadrat')
+
+print('z*z= ', z*z)
