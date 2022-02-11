34ad81cba0fc900653b73af7dafacd6f6f4cf085
Wojciech Balcerzak <wojciech.balcerzak@orpeg.pl>
2022-02-11 07:16:08 +0100

    updating

--- 2021/3LO/kolo_kwardat.py
+++ 2021/3LO/kolo_kwardat.py
@@ -4,21 +4,29 @@ Created on Wed Jan 19 13:59:48 2022
 
 @author: WojciechBalcerzak
 """
-# import turtle
-
-# t = turtle
-# t.pendown()
-
-# for e in range(4):
-#     t.forward(200)
-#     t.left(90)
-# t.goto(100,0)
-# t.circle(100)
-
-# t.penup()
-# turtle.done()
 
 
-lst = [1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 2, 1, 2, 1]
+import turtle
+t = turtle
+t.penup()
+t.setpos(-100,0)
+t.pendown()
+t.circle(100)
+t.penup()
 
-print(sum(lst))
+t.goto(-100,100)
+t.right(45)
+t.fd(100)
+t.left(135)
+t.pendown()
+for e in range(4):
+    t.forward(141)
+    t.left(90)
+t.penup()
+turtle.done()
+
+
+
+# lst = [1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 2, 1, 2, 1]
+
+# print(sum(lst))
