3d472eb942436205d5fc618a0b404624c1e961a5
Wojciech Balcerzak <wojciech.balcerzak@orpeg.pl>
2022-06-20 19:05:13 +0200

    update

--- 2021/1LOR/Lekcja10/test5.cpp
+++ 2021/1LOR/Lekcja10/test5.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
 using namespace std;
-#include <string>
 
-	
 int main(){
-	int counter = 1;
-	int liczba = 1;
-	while(counter<=10){
-		liczba++;
-		if(liczba%2!=0 && liczba%5==0){
-			cout << counter<<". "<<liczba<<endl;
-			counter++;
-		}
-	}
-	
-	return 0;
+char znak = 'a';
+for (int i = 0; i < 26; i++)
+	cout << static_cast<char>(char(znak)+i);
+return 0;
 }
--- 2021/3LO/kod do pk.py
+++ 2021/3LO/kod do pk.py
@@ -4,3 +4,22 @@ Created on Wed Jan 19 11:53:29 2022
 
 @author: WojciechBalcerzak
 """
+import turtle
+t = turtle
+
+
+
+t.pendown()
+
+t.home()
+t.circle(100)
+t.goto(-100,0)
+t.forward(200)
+t.left(90)
+t.forward(200)
+
+
+t.penup()
+
+t.done()
+
