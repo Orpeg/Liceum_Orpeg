062ffcec5c9ed84ca33aac4ae9ea8eef09b4cd9d
Wojciech Balcerzak <wojciech.balcerzak@orpeg.pl>
2023-01-24 19:04:29 +0100

    up

---
+++ 2022/1LO/lekcja5.py
@@ -0,0 +1,105 @@
+# coding: utf-8
+#Python3
+#zmienne liczbowe
+a = 2
+b =3
+print(a+b)
+print(a/b)
+c = 2.5
+print(c)
+print(int(c))
+a,b,c
+a,b,c = (2,3,2.5)
+a
+b
+c
+a,b,c=(4,5,8.5)
+a
+b
+c
+d = 4
+a
+#a = b
+#a == b
+a == d
+pizza = True
+color = 'Green'
+sushi = False
+pizza and sushi
+pizza or sushi
+f = int(input('wpisz liczbę całkowitą  '))
+f
+f = int(input('wpisz liczbę całkowitą  '))
+f = float(input('wpisz liczbę całkowitą  '))
+f
+# and &
+# or |
+pizza
+sushhi
+sushhi
+sushi
+pizza & sushi
+pizza | sushi
+napis = 'Jedzie pociąg z daleka'
+dod = 'az z samego Krakowa'
+napis + dod
+napis + ' ' + dod
+#metody
+napis.upper()
+napis.lower()
+dod
+dod.lower()
+napis.title()
+dod
+napis.startswith('Jedzie')
+napis.startswith('jedzie')
+dod
+dod.endswith('Krakowa')
+dod.endswith('Warszawy')
+dod = '      az z samego      Krakowa')
+dod = '      az z samego      Krakowa'
+dod
+dod.strip()
+dod.strip()
+dod
+dod.strip()
+dod = dod.strip()
+dod
+dod.repace('  ', ' ')
+dod.replace('  ', ' ')
+dod = dod.replace('  ', ' ')
+dodo
+dod
+dod = dod.replace('  ', ' ')
+dod
+dod = dod.replace('  ', ' ')
+dod
+dod
+dod = dod.replace('  ', ' ')
+dod
+#slicing
+napis
+napis[::-1]
+napis[5:10:1]
+napis[5:15:2]
+#lista
+imiona = ['Janek', 'Zosia', 'Małgosia', 'Kinga']
+imiona
+nazwiska = ['Nowak', 'Kowalska', 'Kwiatkowska', 'Kamińska']
+nazwiska
+imiona + ['Magda']
+imiona
+imiona.append('Magda')
+imiona
+imiona
+imiona[0]
+imiona[4]
+imiona[4] = 'Magdalena'
+imiona
+imiona[-1]
+len(imiona)
+imiona[-2]
+tytul = 'w pustyni i w puszczy'
+tytul.uppper()
+tytul.upper()
+tytul.title()
