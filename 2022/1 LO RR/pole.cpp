4a9b5c4e2e15425ad3ab5af4f7977cd9541d8585
Wojciech Balcerzak <wojciech.balcerzak@orpeg.pl>
2023-04-03 19:08:46 +0200

    up

---
+++ 2022/1 LO RR/pole.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	float p, a, b;
+	
+	cout<<"podaj boki trojkata a, b"<<endl;
+	
+	cin>>a;
+	cin >> b;
+	
+	p = 0.5*a*b;
+	
+	cout << "pole trojkata o bokach a="<<a<<"  b = "<<b<<"  wynosi p = "<<p<<endl;
+	
+	
+	
+	
+	return 0;
+	
+}
---
+++ 2022/1 LO RR/program_primo.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+int main() {
+	
+	float x = 0.0;
+	float y = 0.1;
+	
+	std::cout<<"liczba poczatkowa x = 0 , zwiekszamy o y = 0.1"<< std::endl;
+	
+	std::cout << x<<std::endl;
+	x = x + y;
+	
+	std::cout << x<<std::endl;
+	x = x + y;
+	
+	std::cout << x<<std::endl;
+	x = x + y;
+	
+	std::cout << x<<std::endl;
+	x = x + y;
+	
+	std::cout << x<<std::endl;
+	x = x + y;
+	
+		std::cout << x<<std::endl;
+	x = x + y;
+		std::cout << x<<std::endl;
+	x = x + y;
+		std::cout << x<<std::endl;
+	x = x + y;
+		std::cout << x<<std::endl;
+	x = x + y;
+		std::cout << x<<std::endl;
+	x = x + y;
+		std::cout << x<<std::endl;
+	x = x + y;
+		std::cout << x<<std::endl;
+	x = x + y;
+		std::cout << x<<std::endl;
+	x = x + y;
+		std::cout << x<<std::endl;
+	x = x + y;
+	
+	
+	std::cout << x << std::endl;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
